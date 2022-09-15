6a24990996656d0f8167c6816735519e72669075
Oleksii <example.mail.com>
2022-09-15 14:34:34 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
